1d47787619bb682aa8b4a13c3b8fcf7a877da87f
Joshua Edwards <joshuajamesedwards1@gmail.com>
2024-01-24 21:08:34 +0000

    fix js

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -6,7 +6,7 @@
     <title>@yield('title', 'Joshua Edwards Tutoring')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite('resources/sass/app.scss', 'resources/js/app.js')
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     @yield('scripts')
 </head>
 <body class="background">
