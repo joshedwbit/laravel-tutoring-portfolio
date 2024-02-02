f307eab46173449db24b15ea1ccfba6c66fe1473
Joshua Edwards <joshuajamesedwards1@gmail.com>
2024-02-02 00:02:20 +0000

    add aos module

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,8 @@
+import AOS from "aos";
+import 'aos/dist/aos.css';
 import { navDropdown } from "./navbar";
 import { twitterDropdown } from "./twitter-dropdown";
 
+AOS.init();
 navDropdown();
 twitterDropdown();
--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -11,6 +11,7 @@
     <link href="https://fonts.cdnfonts.com/css/callie-chalk-font" rel="stylesheet">
     <link href="https://fonts.cdnfonts.com/css/djb-chalk-it-up" rel="stylesheet">
     <link rel="icon" type="image/x-icon" href="{{ asset('images/favicon/initials-icon.ico') }}">
+    <link rel="stylesheet" href="bower_components/aos/dist/aos.css" />
 </head>
 <body>
     <header>
