a77c2d6abd8be36d25624b91c10aeaf2d26736dc
Joshua Edwards <joshuajamesedwards1@gmail.com>
2024-08-16 14:29:53 +0100

    styling tweaks

--- resources/views/resources.blade.php
+++ resources/views/resources.blade.php
@@ -4,62 +4,47 @@
 
 @section('content')
 <h1 class="page-title">{{ $pageInfo['title'] }}</h1>
-{{--
-    <div class="reviews__item">
-        <h4>Year</h4>
-    </div>
 
-    <div class="reviews__item">
-        <h4>Paper Number</h4>
-    </div>
 
-    <div class="reviews__item">
-        <h4>Season</h4>
-    </div>
+<section class="resources--container">
+    <h4 class="resources__header">Year</h4>
+    <h4 class="resources__header">Paper</h4>
+    <h4 class="resources__header">Season</h4>
+    <h4 class="resources__header">Type</h4>
+    <h4 class="resources__header">Level</h4>
+    <h4 class="resources__header">Question</h4>
+    <h4 class="resources__header">Topic(s)</h4>
 
-    <div class="reviews__item">
-        <h4>Calculator</h4>
-    </div>
+    @foreach ($papers as $paper)
+        <div>
+            {{ $paper['year'] }}
+        </div>
 
-    <div class="reviews__item">
-        <h4>higher</h4>
-    </div>
+        <div>
+            {{ $paper['paper_number'] }}
+        </div>
 
-    <div class="reviews__item">
-        <h4>question number</h4>
-    </div>
+        <div>
+            {{ $paper['season'] }}
+        </div>
 
-    <div class="reviews__item">
-        <h4>Topic(s)</h4>
-    </div> --}}
-@foreach ($papers as $paper)
-    <div class="reviews__item">
-        {{ $paper['year'] }}
-    </div>
+        <div>
+            {{ $paper['calculator'] ? 'calculator' : 'non-calc' }}
+        </div>
 
-    <div class="reviews__item">
-        {{ $paper['paper_number'] }}
-    </div>
+        <div>
+            {{ $paper['higher'] ? 'higher' : 'foundation' }}
+        </div>
 
-    <div class="reviews__item">
-        {{ $paper['season'] }}
-    </div>
+        <div>
+            {{ $paper['question_number'] }}
+        </div>
 
-    <div class="reviews__item">
-        {{ $paper['calculator'] ? 'calculator' : 'non-calculator' }}
-    </div>
-
-    <div class="reviews__item">
-        {{ $paper['higher'] ? 'higher' : 'foundation' }}
-    </div>
-
-    <div class="reviews__item">
-        {{ $paper['question_number'] }}
-    </div>
-
-    <div class="reviews__item">
-        {{ $paper['topic'] }}
-    </div>
+        <div>
+            {{ $paper['topic'] }}
+        </div>
     @endforeach
+
+    </section>
 
 @endsection
