eb94318309b6fd5dd4dcb1eb9d48792bb67ac1bb
Joshua Edwards <joshuajamesedwards1@gmail.com>
2024-08-24 15:16:23 +0100

    update format of contact number

--- config/contact.php
+++ config/contact.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'mobile' => '+44 78 0822 6490',
+    'mobile' => '+44 7808 226490',
     'email' => 'joshuajamesedwards1@gmail.com',
     'linkedin' => 'https://www.linkedin.com/in/joshua-edwards-4347329b/',
 ];
