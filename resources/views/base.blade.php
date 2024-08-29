3d4b3de06ddc693c732668607e8d7920ad461220
Joshua Edwards <joshuajamesedwards1@gmail.com>
2024-08-29 09:41:13 +0100

    rearrange meta tags

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -2,8 +2,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
     <title>@yield('title', 'Joshua Edwards Tutoring')</title>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
