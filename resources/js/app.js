438c2b3b1eb78fc249ce33482ae4f2ccb5214972
Joshua Edwards <joshuajamesedwards1@gmail.com>
2024-08-30 16:46:59 +0100

    setup slick carousel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,17 @@
 import { navDropdown } from "./navbar";
 import $ from 'jquery';
 window.$ = window.jQuery = $;
+import 'slick-carousel';
 
 navDropdown();
+
+$(document).ready(function(){
+    $('.your-carousel-class').slick({
+        // Slick carousel options
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+    });
+});
