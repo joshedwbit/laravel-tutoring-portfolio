2d94d8e214c5807ff5ccaa890c8c92de6d5b425d
Joshua Edwards <joshuajamesedwards1@gmail.com>
2024-08-31 19:35:07 +0100

    apply AOS to reviews page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,15 @@
 import AOS from "aos";
 import { navDropdown } from "./navbar";
 
-AOS.init();
+AOS.init({
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    anchorPlacement: 'bottom-bottom', // defines which position of the element regarding to window should trigger the animation
+    offset: 100, // offset (in px) from the original trigger point
+    //delay: 1000, // values from 0 to 3000, with step 50ms
+    duration: 500, // values from 0 to 3000, with step 50ms
+    easing: 'ease-in', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: true, // whether elements should animate out while scrolling past them
+});
+
 navDropdown();
--- resources/views/reviews.blade.php
+++ resources/views/reviews.blade.php
@@ -7,7 +7,7 @@
 
 
 @foreach ($reviews as $review)
-<section class="reviews reviews--container">
+<section class="reviews reviews--container" data-aos="fade-up">
     <div class="reviews__item">
         <h4>{{ $pageInfo['label_added'] }}</h4>
         {{ $review['created_at'] }}
