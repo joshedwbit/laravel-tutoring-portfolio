67e38b2facbc09f7b418b4c30dc538781dca5568
Joshua Edwards <joshuajamesedwards1@gmail.com>
2024-09-06 15:21:37 +0100

    apply AOS once and not repeated

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,7 +8,7 @@ AOS.init({
     //delay: 1000, // values from 0 to 3000, with step 50ms
     duration: 500, // values from 0 to 3000, with step 50ms
     easing: 'ease-in', // default easing for AOS animations
-    once: false, // whether animation should happen only once - while scrolling down
+    once: true, // whether animation should happen only once - while scrolling down
     mirror: true, // whether elements should animate out while scrolling past them
 });
 
