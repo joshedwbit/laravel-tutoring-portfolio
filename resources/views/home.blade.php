34df736d5c80d6b604f52b7c81c4d1a8d5b2ff4c
Joshua Edwards <joshuajamesedwards1@gmail.com>
2024-10-21 23:54:24 +0100

    move carousel to partial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -30,19 +30,7 @@
             <p>If interested please <a class="homepage__link" href="/contact">get in contact.</a></p>
         </div>
     </div>
+
+    @include('partials._feedback-success-carousel')
 </section>
-
-<div class="swiper">
-    <div class="swiper-wrapper swiper-wrapper-centered">
-        <div class="swiper-slide"><img src="{{ asset('images/png/a2-a.png') }}" alt="a2 grade a" title="a2 grade a"></div>
-        <div class="swiper-slide"><img src="{{ asset('images/png/a2-b.png') }}" alt="a2 grade b" title="a2 grade b"></div>
-        <div class="swiper-slide"><img src="{{ asset('images/png/gcse-5.png') }}" alt="gcse grade 5" title="gcse grade 5"></div>
-        <div class="swiper-slide"><img src="{{ asset('images/png/gcse-5(2).png') }}" alt="gcse grad 5" title="gcse grad 5"></div>
-        <div class="swiper-slide"><img src="{{ asset('images/png/gcse-6.png') }}" alt="gcse grade 6" title="gcse grade 6"></div>
-        <div class="swiper-slide"><img src="{{ asset('images/png/gcse-6(2).png') }}" alt="gcse grade 6" title="gcse grade 6"></div>
-        <div class="swiper-slide"><img src="{{ asset('images/png/gcse-8.png') }}" alt="gcse grade 8" title="gcse grade 8"></div>
-    </div>
-
-    <div class="swiper-pagination"></div>
-</div>
 @endsection
