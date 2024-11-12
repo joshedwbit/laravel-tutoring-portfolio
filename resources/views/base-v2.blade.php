4dc0fc2ee644bc8cbeaa1e1498f7b3bdfbe14b96
Joshua Edwards <joshuajamesedwards1@gmail.com>
2024-11-12 23:07:11 +0000

    initial styling changes (without affecting existing styling)

---
+++ resources/views/base-v2.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" class="html-v2">
+<head>
+    <title>@yield('title', 'Joshua Edwards Tutoring')</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="@yield('meta_description', 'Welcome to Joshua Edwards tutoring portfolio. Discover personalised math tutoring services designed to help students achieve their academic goals. Explore resources, read reviews, and find out how I can support your learning journey.')">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="robots" content="@yield('robots_meta', 'index, follow')">
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @yield('scripts')
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/favicon/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicon/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('images/favicon/site.webmanifest') }}">
+</head>
+<body class="body-v2">
+    <header class="js-header">
+        @include('partials._navbar')
+        @include('partials._logo')
+    </header>
+
+    <main class="br">
+        @yield('content')
+    </main>
+
+    <footer>
+        @include('partials._footer')
+    </footer>
+</body>
+</html>
--- resources/views/home-v2.blade.php
+++ resources/views/home-v2.blade.php
@@ -1,4 +1,4 @@
-@extends('base')
+@extends('base-v2')
 
 @section('title', 'Home - Joshua Edwards Tutoring')
 @section('meta_description', 'Welcome to Joshua Edwards Tutoring! I specialise in personalised maths tutoring for KS3 to A-level students, with a proven track record of outstanding results. My aim is to support dedicated learners in achieving their academic goals and exceeding their expectations. Learn how personalised tutoring can help you.')
