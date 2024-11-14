68321e3afcf2be38cca46fd5510830da13449562
Joshua Edwards <joshuajamesedwards1@gmail.com>
2024-11-14 16:00:04 +0000

    main body more groundwork

--- resources/views/home-v2.blade.php
+++ resources/views/home-v2.blade.php
@@ -4,5 +4,17 @@
 @section('meta_description', 'Welcome to Joshua Edwards Tutoring! I specialise in personalised maths tutoring for KS3 to A-level students, with a proven track record of outstanding results. My aim is to support dedicated learners in achieving their academic goals and exceeding their expectations. Learn how personalised tutoring can help you.')
 
 @section('content')
-stuff
+<div class="v2homepage__name">Joshua Edwards</div>
+<div class="v2homepage__job-title">Math tutor</div>
+<div class="v2homepage__education-title">Maths Masters graduate from the University of Nottingham</div>
+
+{{-- image here  --}}
+<div class="v2homepage__current-information">I currently work as a software developer and in my free time I tutor mathematics</div>
+<div class="v2homepage__background">The class of 2024/2025 will be my fifth consecutive year one on one tutoring.  My aim is to motivate students of all
+     levels to work hard to achieve desired grades and beyond.</div>
+<div class="v2homepage__read-more">Continue reading</div>
+{{-- reviews here  --}}
+<div class="">Contact</div>
+
+
 @endsection
