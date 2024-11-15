00c85671a4c64b2021709e8705a08390e1f5b7df
Joshua Edwards <joshuajamesedwards1@gmail.com>
2024-11-15 16:45:53 +0000

    fix typo in alt tag on feedback success carousel

--- resources/views/partials/_feedback-success-carousel.blade.php
+++ resources/views/partials/_feedback-success-carousel.blade.php
@@ -7,7 +7,7 @@
         <div class="swiper-slide"><img src="{{ asset('images/png/a2-a.png') }}" alt="a2 grade a" title="a2 grade a"></div>
         <div class="swiper-slide"><img src="{{ asset('images/png/a2-b.png') }}" alt="a2 grade b" title="a2 grade b"></div>
         <div class="swiper-slide"><img src="{{ asset('images/png/gcse-5.png') }}" alt="gcse grade 5" title="gcse grade 5"></div>
-        <div class="swiper-slide"><img src="{{ asset('images/png/gcse-5(2).png') }}" alt="gcse grad 5" title="gcse grad 5"></div>
+        <div class="swiper-slide"><img src="{{ asset('images/png/gcse-5(2).png') }}" alt="gcse grade 5" title="gcse grad 5"></div>
         <div class="swiper-slide"><img src="{{ asset('images/png/gcse-6.png') }}" alt="gcse grade 6" title="gcse grade 6"></div>
         <div class="swiper-slide"><img src="{{ asset('images/png/gcse-6(2).png') }}" alt="gcse grade 6" title="gcse grade 6"></div>
         <div class="swiper-slide"><img src="{{ asset('images/png/gcse-8.png') }}" alt="gcse grade 8" title="gcse grade 8"></div>
