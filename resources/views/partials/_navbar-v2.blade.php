0c46b95c0b2135d609d2153d89614376feb2f847
Joshua Edwards <joshuajamesedwards1@gmail.com>
2024-11-24 23:14:16 +0000

    tweak navbar href links

--- resources/views/partials/_navbar-v2.blade.php
+++ resources/views/partials/_navbar-v2.blade.php
@@ -6,9 +6,9 @@
     <div class="v2header__navbar">
         {{-- todo implement toggle js  --}}
         <a href="/new-home" class="v2header__navbar-item active">Home</a>
-        <a href="#section--reviews" class="v2header__navbar-item">Reviews</a>
-        <a href="#section--contact" class="v2header__navbar-item">Contact</a>
-        <a href="#section--links" class="v2header__navbar-item">Links</a>
+        <a href="/new-home#section--reviews" class="v2header__navbar-item">Reviews</a>
+        <a href="/new-home#section--contact" class="v2header__navbar-item">Contact</a>
+        <a href="/new-home#section--links" class="v2header__navbar-item">Links</a>
     </div>
 
     <a href="#section--contact" class="button v2header__contact-button button--standard">
