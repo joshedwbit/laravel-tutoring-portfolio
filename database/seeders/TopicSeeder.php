0de08fd00aaac935278c45c90ebee12da2792f4e
Joshua Edwards <joshuajamesedwards1@gmail.com>
2025-04-15 23:48:09 +0100

    add Topic enum

--- database/seeders/TopicSeeder.php
+++ database/seeders/TopicSeeder.php
@@ -16,7 +16,6 @@ class TopicSeeder extends Seeder
     public function run(): void
     {
         foreach (TopicSeeder::getTopics() as $topic) {
-            var_dump($topic);
             Topics::create([
                 'topic' => $topic
             ]);
