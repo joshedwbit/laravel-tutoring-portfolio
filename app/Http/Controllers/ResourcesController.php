389f30fec76f44c0695c1e6979241ff4de636aa3
Joshua Edwards <joshuajamesedwards1@gmail.com>
2025-04-17 10:45:17 +0100

    20 items per page rather than 10

--- app/Http/Controllers/ResourcesController.php
+++ app/Http/Controllers/ResourcesController.php
@@ -36,7 +36,7 @@ class ResourcesController extends Controller
     {
         return view('resources-v2', [
             'pageInfo' => Resources::first(),
-            'papers' => Papers::orderBy('year', 'desc')->simplePaginate(10),
+            'papers' => Papers::orderBy('year', 'desc')->simplePaginate(20),
             'results_count' => Papers::count(),
             'topics' => Topics::all(),
             'filtered' => false,
@@ -154,7 +154,7 @@ class ResourcesController extends Controller
         // dd($query->toSql(), $query->getBindings());
         // total must be calculated before filtering so the order below is important
         $filteredResourcesTotal = count($query->get());
-        $filteredResources = $query->simplePaginate(10)->withQueryString();
+        $filteredResources = $query->simplePaginate(20)->withQueryString();
 
         return view('resources-v2', [
             'pageInfo' => Resources::first(),
